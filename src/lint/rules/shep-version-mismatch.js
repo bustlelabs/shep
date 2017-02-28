2d0fc7cd1eaecc4db39d850df6f0f4bb395ce02b
Chris Olszewski <chrisdolszewski@gmail.com>
2017-02-28 13:43:58 -0600

    add shep version mismatch rule

---
+++ src/lint/rules/shep-version-mismatch.js
@@ -0,0 +1,11 @@
+import { version } from '../../index'
+import * as load from '../../util/load'
+
+export default function () {
+  const pkgVersion = load.pkg().devDependencies.shep
+  return pkgVersion === version ? [] : generateWarning(version, pkgVersion)
+}
+
+function generateWarning (version, pkgVersion) {
+  return { rule: 'shep-version-mismatch', message: `shep ${version} is being used, but package.json requires ${pkgVersion}` }
+}
