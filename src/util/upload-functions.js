4dc1ba0517b7854405bbe7b8850546b1a96cb15b
Chris Olszewski <chrisdolszewski@gmail.com>
2017-10-17 13:30:40 -0500

    fix logic for new aliases on existing functions

--- src/util/upload-functions.js
+++ src/util/upload-functions.js
@@ -1,13 +1,13 @@
 import Promise from 'bluebird'
 import merge from 'lodash.merge'
-import { updateFunction, getFunction, isFunctionDeployed, createFunction } from './aws/lambda'
+import { updateFunction, getFunction, isFunctionDeployed, doesAliasExist, createFunction } from './aws/lambda'
 import { lambdaConfig, distPath } from './load'
 import zipDir from './zip-dir'
 
 export default async function (fns, env) {
   return Promise.map(fns, async ({ name, key, bucket }) => {
     const config = await lambdaConfig(name)
-    let wantedFunc = { Config: config, Code: {} }
+    let wantedFunc = { Config: config, Code: {}, Identifier: {} }
 
     if (bucket && key) {
       wantedFunc.Code.s3 = { bucket, key }
@@ -16,8 +16,13 @@ export default async function (fns, env) {
       wantedFunc.Code.Zip = await zipDir(path)
     }
 
+    // possible cases
+    // completely new func, new alias, update version
+
     if (await isFunctionDeployed(config.FunctionName)) {
-      const oldFunc = await getFunction({ FunctionName: config.FunctionName, Qualifier: env })
+      const aliasExists = await doesAliasExist({ FunctionName: config.FunctionName, Alias: env })
+      const oldFunc = await getFunction({ FunctionName: config.FunctionName, Qualifier: aliasExists ? env : undefined })
+      if (!aliasExists) { wantedFunc.Identifier.Alias = env }
       wantedFunc.Config.Environment = merge({}, oldFunc.Config.Environment, wantedFunc.Config.Environment)
       return updateFunction(oldFunc, wantedFunc)
     } else {
