c076e15078569cbf46a0484fc152235048aecdf2
dmabuada <abuadas.dalia@gmail.com>
2018-07-05 14:19:05 -0400

    Create bucket if value exists but bucket doesn't

--- src/util/upload-builds.js
+++ src/util/upload-builds.js
@@ -2,7 +2,7 @@ import Promise from 'bluebird'
 import hashBuild from './hash-build'
 import { funcs, distPath } from './load'
 import zipDir from './zip-dir'
-import { putBuild, buildExists } from './aws/s3'
+import { putBuild, buildExists, bucketExists, putBucket } from './aws/s3'
 
 // remind me to fix this or make it more clean or something
 export default async function (pattern, bucket) {
@@ -12,6 +12,10 @@ export default async function (pattern, bucket) {
     const zip = await zipDir(path)
     const hash = hashBuild(zip)
     const key = `${func}-${hash}.zip`
+
+    if (!await bucketExists(key, bucket)) {
+      await putBucket(key, bucket, zip)
+    }
 
     if (!await buildExists(key, bucket)) {
       await putBuild(key, bucket, zip)
